ac036dfc5eebf59e66b6d9e9f0f86dd1e2966bf0
Shivani V <shivani.venk@gmail.com>
2021-07-22 16:54:01 +0000

    read keys n values from database

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -4,6 +4,8 @@ const getMessages = () => {
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val()
-        console.log(data)
+        for (let key in data) {
+            console.log(key, data[key])
+        }
     })
 }
