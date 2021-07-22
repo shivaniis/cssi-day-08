6d9f4957a2b2352d053f1dddb88c6bc4710c604f
Shivani V <shivani.venk@gmail.com>
2021-07-22 18:13:30 +0000

    works

---
+++ public/js/writeMessage.js
@@ -0,0 +1,13 @@
+const onSubmit = () => {
+    console.log("submit button clicked")
+    const passcodeInput = document.querySelector("#passcode").value;
+    const messageInput = document.querySelector("#message").value;
+    console.log(passcodeInput, messageInput)
+
+    const payload = {
+        passcode: passcodeInput,
+        message: messageInput
+    };
+
+    firebase.database().ref().push(payload);
+}
