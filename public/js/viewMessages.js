a6f10002cdb8686087c9b9f167e429312d20d8f8
Shivani V <shivani.venk@gmail.com>
2021-07-22 17:15:02 +0000

    added error message when passcode doesn't have a corresponding value

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -4,13 +4,18 @@ const getMessages = () => {
     const messagesRef = firebase.database().ref();
     messagesRef.on('value', (snapshot) => {
         const data = snapshot.val()
+        let there = false
         for (let key in data) {
-            console.log(key, data[key])
             if (key === passcode.value) {
                 console.log("match found")
+                there = true
                 const message = document.querySelector("#message")
                 message.innerHTML = data[key]
             }
+        }
+        if (!there) {
+            const message = document.querySelector("#message")
+            message.innerHTML = "ERROR: No Message Found"
         }
     })
 }
